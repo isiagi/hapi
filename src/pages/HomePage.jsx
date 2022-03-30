23274c260ae2ea099f8617798f9bed8ba9591c7a
isiagi <isiagigeofrey0@gmail.com>
2022-03-30 12:01:42 +0300

    adds outsource

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -5,6 +5,7 @@ import Nic from '../components/nic/Nic'
 import Offer from '../components/offer/Offer'
 import Why from '../components/why/Why'
 import Contact from '../components/homeContact/Home'
+import Out from '../components/out/Out'
 
 const HomePage = () => {
   return (
@@ -14,6 +15,7 @@ const HomePage = () => {
       <Why />
       <Nic />
       <Inter />
+      <Out />
       <Contact />
     </div>
   )
